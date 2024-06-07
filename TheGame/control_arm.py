a1ecef808e5e44c4dd339d181dea3f187c93c609
The-Sch-O <118555410+The-Sch-O@users.noreply.github.com>
2024-06-07 11:06:07 +0200

    Update control_arm.py
    Clean code

--- TheGame/control_arm.py
+++ TheGame/control_arm.py
@@ -2,10 +2,6 @@ from robomaster import robot
 import time
 
 
-def sub_data_handler(sub_info):
-    pos_x, pos_y = sub_info
-    print("Robotic Arm: pos x:{0}, pos y:{1}".format(pos_x, pos_y))
-
 def initiate_pos(ep_robot):
     """Position par défaut de la pince en l'air"""
     ep_arm = ep_robot.robotic_arm
@@ -39,5 +35,6 @@ def drop_ball(ep_robot):
     ep_arm.move(x=100, y=0).wait_for_completed()
     time.sleep(0.2)
     ep_gripper.open(power=50)
+
     time.sleep(2)
     ep_gripper.pause()

101e4c84a0eafacf4f41d65eb1fac2fa0e1161c6
comoxx <93337725+comoxx@users.noreply.github.com>
2024-06-07 10:55:19 +0200

    Update connect.py

--- TheGame/connect.py
+++ TheGame/connect.py
@@ -2,7 +2,7 @@ from robomaster import robot
 
 def connect(conn_type, sn):
     ep_robot = robot.Robot()
-    if conn_type == "ap":
+    if conn_type == "ap": #Connection via wifi
        ep_robot.initialize(conn_type="ap")
     else:
         ep_robot.initialize(conn_type="sta", sn="3JKCH8800100UF")

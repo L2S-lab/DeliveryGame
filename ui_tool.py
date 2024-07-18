a57c5ba99aba6518d8a3f546cc6a438c30ec58e1
Aarsh Thakker <aarsh.thakker@centralesupelec.fr>
2024-07-18 12:12:40 +0200

    minor changes

--- ui_tool.py
+++ ui_tool.py
@@ -61,7 +61,7 @@ class UI:
     def main(self):
         global SILENT_MODE, LAMGUAGE, NB_BALLS, CONN_TYPE, SERIAL_NUMBER, KP_X, KI_X, KP_Y, KI_Y
         root = tk.Tk()
-        root.title("Jeu de Livraison")
+        root.title("Delivery game")
 
         width=610
         height=500
@@ -73,11 +73,19 @@ class UI:
 
         GLabel_362=tk.Label(root)
         GLabel_362["bg"] = "#ffffff"
-        ft = tkFont.Font(family='Times',size=14)
+        ft = tkFont.Font(family='Times',size=14, weight='bold')
         GLabel_362["font"] = ft
         GLabel_362["fg"] = "#333333"
         GLabel_362["justify"] = "center"
-        GLabel_362["text"] = "Réglage des coefficients du correcteur PI et d'autres variables"
+        GLabel_362.place(x=0,y=0,width=610,height=60)
+
+        GLabel_362=tk.Label(root)
+        GLabel_362["bg"] = "#ffffff"
+        ft = tkFont.Font(family='Times',size=14, weight='bold')
+        GLabel_362["font"] = ft
+        GLabel_362["fg"] = "#333333"
+        GLabel_362["justify"] = "center"
+        GLabel_362["text"] = "PI tuning parameters"
         GLabel_362.place(x=0,y=60,width=610,height=35)
 
         ft = tkFont.Font(family='Times',size=12)
@@ -103,7 +111,7 @@ class UI:
         GLabel_kix["font"] = ft
         GLabel_kix["fg"] = "#333333"
         GLabel_kix["justify"] = "center"
-        GLabel_kix["text"] = "KP_x"
+        GLabel_kix["text"] = "KI_x"
         GLabel_kix.place(x=135,y=105,width=65,height=18)
 
         self.ki_x = tk.StringVar(root,value="0.038")
@@ -167,34 +175,42 @@ class UI:
         #entry_kiy["relief"] = "groove"
         entry_kiy.place(x=450,y=105,width=65,height=18)
 
+        GLabel_362=tk.Label(root)
+        GLabel_362["bg"] = "#ffffff"
+        GLabel_362["font"] = ft
+        GLabel_362["fg"] = "#333333"
+        GLabel_362["justify"] = "center"
+        GLabel_362["text"] = "Other options"
+        GLabel_362.place(x=0,y=150,width=610,height=22)
+
         GLabel_lang=tk.Label(root)
         GLabel_lang["font"] = ft
         GLabel_lang["fg"] = "#333333"
-        GLabel_lang["justify"] = "center"
-        GLabel_lang["text"] = "Select Language"
-        GLabel_lang.place(x=15,y=150,width=110,height=18)
+        GLabel_lang["justify"] = "left"
+        GLabel_lang["text"] = "Audio"
+        GLabel_lang.place(x=-20,y=230,width=110,height=18)
         self.lang = tk.StringVar(root)
         self.lang.set("silent")
         Radio_fr = tk.Radiobutton(root, text="FR", variable=self.lang, value="fr")
-        Radio_fr.place(x=120, y=150)
+        Radio_fr.place(x=120, y=230)
         Radio_en = tk.Radiobutton(root, text="EN", variable=self.lang, value="en")
-        Radio_en.place(x=170, y=150)
+        Radio_en.place(x=170, y=230)
         Radio_silent = tk.Radiobutton(root, text="Silent", variable=self.lang, value="silent")
-        Radio_silent.place(x=220, y=150)
+        Radio_silent.place(x=220, y=230)
 
         GLabel_balls=tk.Label(root)
         GLabel_balls["font"] = ft
         GLabel_balls["fg"] = "#333333"
-        GLabel_balls["justify"] = "center"
+        GLabel_balls["justify"] = "left"
         GLabel_balls["text"] = "Number of balls"
-        GLabel_balls.place(x=5,y=190,width=130,height=18)
+        GLabel_balls.place(x=0,y=190,width=130,height=18)
 
         self.nb_balls = tk.StringVar(root,value="2")
         entry_balls=tk.Entry(root, textvariable=self.nb_balls)
         entry_balls["borderwidth"] = "1px"
         entry_balls["font"] = ft
         entry_balls["fg"] = "#333333"
-        entry_balls["justify"] = "center"
+        entry_balls["justify"] = "left"
         #entry_balls["text"] = "nb_balls"
         #entry_balls["relief"] = "groove"
         entry_balls.place(x=140,y=190,width=65,height=18)
@@ -202,54 +218,53 @@ class UI:
         GLabel_conn=tk.Label(root)
         GLabel_conn["font"] = ft
         GLabel_conn["fg"] = "#333333"
-        GLabel_conn["justify"] = "center"
-        GLabel_conn["text"] = "Connection Type"
-        GLabel_conn.place(x=0,y=230,width=150,height=18)
+        GLabel_conn["justify"] = "left"
+        GLabel_conn["text"] = "Connection type"
+        GLabel_conn.place(x=-10 ,y=265,width=150,height=18)
         self.conn = tk.StringVar(root)
         self.conn.set("ap")
         Radio_ap = tk.Radiobutton(root, text="Direct", variable=self.conn, value="ap")
-        Radio_ap.place(x=130, y=230)
+        Radio_ap.place(x=130, y=265)
         Radio_sta = tk.Radiobutton(root, text="Router", variable=self.conn, value="sta")
-        Radio_sta.place(x=190, y=230)
+        Radio_sta.place(x=190, y=265)
 
         GLabel_sn=tk.Label(root)
         GLabel_sn["font"] = ft
         GLabel_sn["fg"] = "#333333"
-        GLabel_sn["justify"] = "center"
-        GLabel_sn["text"] = "Serial number"
-        GLabel_sn.place(x=0,y=270,width=130,height=18)
+        GLabel_sn["justify"] = "left"
+        GLabel_sn["text"] = "Robot serial number*"
+        GLabel_sn.place(x=15,y=305,width=130,height=18)
 
         #self.sn = tk.StringVar(root)
         self.entry_sn=tk.Entry(root)
         self.entry_sn["borderwidth"] = "1px"
         self.entry_sn["font"] = ft
         self.entry_sn["fg"] = "#333333"
-        self.entry_sn["justify"] = "center"
+        self.entry_sn["justify"] = "left"
         #self.entry_balls["text"] = "sn"
         #self.entry_balls["relief"] = "groove"
-        self.entry_sn.place(x=130,y=270,width=130,height=18)
+        self.entry_sn.place(x=150,y=305,width=130,height=18)
 
         GButton_start=tk.Button(root)
         GButton_start["bg"] = "#efefef"
         GButton_start["font"] = ft
         GButton_start["fg"] = "#000000"
-        GButton_start["justify"] = "center"
+        GButton_start["justify"] = "left"
         GButton_start["text"] = "Start"
-        GButton_start.place(x=250,y=300,width=122,height=48)
+        GButton_start.place(x=250,y=335,width=100,height=38)
         GButton_start["command"] = self.get_values
         
         msg = '''
-    - Lors de l'utilisation de la connexion par routeur, il est nécessaire d'indiquer le numéro de série correct du robot. Il est généralement composé de 14 caractères alphanumériques (vous pouvez le trouver sur le robot).\n
-    - Le nombre de balles peut être de -1 dans le cas d'une boucle infinie (non recommandé).\n
-    Rappel : Si on augmente la valeur de Kp, on gagne en vitesse mais on perd en précision. Si on augmente la valeur de Ki, on perd en précision et en stabilité. 
+    * When using the router connection, you need to specify the robot's correct serial number. This is generally made up of 14 alphanumeric characters written on the robot.
         '''
+    #- The number of balls can be -1 in the case of an infinite loop (not recommended).\n
+    # Reminder: If you increase the value of Kp, you gain speed but lose precision. If you increase the value of Ki, you lose precision and stability.  
         GMessage_info=tk.Label(root, wraplength=550)
-        ft = tkFont.Font(family='Times',size=10)
-        GMessage_info["font"] = ft
+        GMessage_info["font"] = tkFont.Font(family='Times',size=10, slant='italic')
         GMessage_info["fg"] = "#333333"
         GMessage_info["justify"] = "left"
         GMessage_info["text"] = msg
-        GMessage_info.place(x=20,y=350,height=160)
+        GMessage_info.place(x=20,y=380,height=80)
 
         image = tk.PhotoImage(file="assets/CS.png")
         image_resized = image.subsample(8, 8)  # Les arguments sont les facteurs de réduction pour la largeur et la hauteur
